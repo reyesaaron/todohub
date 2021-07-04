52261a33d11d40b3a439cbd581464000c8802bdd
reyesaaron <theaaronreyes02@gmail.com>
2021-07-04 21:08:11 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import Navigator from './routes/drawer'
+
+export default function App() {
+  return (
+    <Navigator />
+  );
+}
+
+const styles = StyleSheet.create({
+
+})
